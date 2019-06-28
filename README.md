a33187a21437538a9ae0045d4ae13da9e15a46e4
Andy Bulka <abulka@gmail.com>
2019-06-28 11:16:19 +1000

    button click starts to work

---
+++ README.md
@@ -0,0 +1,7 @@
+wx_esper
+========
+
+An attempt to use esper ECS for traditional GUI programming.
+
+Wish me luck!
+
--- main.py
+++ main.py
@@ -12,9 +12,27 @@ class Render:
     info: str = ""
 
 
+@dataclass
+class Dirty:
+    """Mark that component needs rendering."""
+
+
+class RenderProcessor(esper.Processor):
+    def __init__(self):
+        super().__init__()
+
+    def process(self):
+        print("RENDER")
+        for ent, (render, dirty) in self.world.get_components(Render, Dirty):
+            print(f"entity {ent} says {render.info}")
+
 class MyFrame1A(MyFrame1):
     def onButton1(self, event):
+        # global world
+
         print("button pushed")
+        world.process()
+
 
     # def OnHtml2(self, event):
     #     url = "http://www.wxpython.org/docs/api/wx.html.HtmlWindow-class.html"
@@ -25,6 +43,19 @@ class MyFrame1A(MyFrame1):
     #     wx.CallAfter(frame.m_htmlWin1.LoadPage, url)
     # frame.m_htmlWin1.LoadPage(url)
 
+# def setup_esper():
+world = esper.World()
+world.add_processor(RenderProcessor())
+
+gui_title = world.create_entity()
+gui_textentry = world.create_entity()
+gui_checkbox = world.create_entity()
+gui_button1 = world.create_entity()
+
+world.add_component(gui_title, Render())
+world.add_component(gui_title, Dirty())
+
+# setup_esper()
 
 app = wx.App()
 frame = MyFrame1A(None)
