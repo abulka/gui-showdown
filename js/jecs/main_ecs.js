92a143af667762581fbf56228f081d958f36cefe
Andy Bulka <abulka@gmail.com>
2019-07-08 19:51:11 +1000

    restored

--- js/jecs/main_ecs.js
+++ js/jecs/main_ecs.js
@@ -36,13 +36,13 @@ class GuiControlRef {  // Mediator (entity + this component) needs to know about
 
 class Flag {}  // Mediator (entity + this component) might have a flag to indicate some behaviour is wanted
 
-class DisplayOptions {
-  constructor() {
-    this.uppercase_welcome = false
-    this.uppercase_user = false
-    this.uppercase_welcome_user = false
-  }
-}
+// class DisplayOptions {
+//   constructor() {
+//     this.uppercase_welcome = false
+//     this.uppercase_user = false
+//     this.uppercase_welcome_user = false
+//   }
+// }
 
 //
 // Wire up and build everything
@@ -51,7 +51,7 @@ class DisplayOptions {
 const entity_welcome_left = world.entity('entity_welcome_left')
 entity_welcome_left.setComponent('c_model_ref', new ModelRef(model, ['welcomemsg']))
 entity_welcome_left.setComponent('c_gui_div', new GuiControlRef('welcome'))  // id of div to hold welcome message, top left
-entity_welcome_left.setComponent('c_display_options', new DisplayOptions())
+// entity_welcome_left.setComponent('c_display_options', new DisplayOptions())
 
 const entity_welcome_user_right = world.entity('entity_welcome_user_right')
 entity_welcome_user_right.setComponent('c_multi_model_ref', new MultiModelRef(
@@ -62,7 +62,7 @@ entity_welcome_user_right.setComponent('c_multi_model_ref', new MultiModelRef(
   ]
 ));
 entity_welcome_user_right.setComponent('c_gui_div', new GuiControlRef('welcome-user'));  // id of div to hold welcome + user message, top right
-entity_welcome_user_right.setComponent('c_display_options', new DisplayOptions())
+// entity_welcome_user_right.setComponent('c_display_options', new DisplayOptions())
 
 const entity_edit_welcome_msg = world.entity('entity_edit_welcome_msg')
 entity_edit_welcome_msg.setComponent('c_model_ref', new ModelRef(model, ['welcomemsg']));
@@ -171,8 +171,8 @@ $('#reset_user_model').on('click', function(e) {
 })
 
 $("input[name=uppercase_welcome]").change(function(e) {
-  entity_welcome_left.components.c_display_options.uppercase_welcome = $(e.target).prop('checked')
-  entity_welcome_user_right.components.c_display_options.uppercase_welcome = $(e.target).prop('checked')
+  // entity_welcome_left.components.c_display_options.uppercase_welcome = $(e.target).prop('checked')
+  // entity_welcome_user_right.components.c_display_options.uppercase_welcome = $(e.target).prop('checked')
   
   add_or_remove_component(world, 
     $(e.target).prop('checked'), 
@@ -183,7 +183,7 @@ $("input[name=uppercase_welcome]").change(function(e) {
 })
 
 $("input[name=uppercase_user]").change(function(e) {
-  entity_welcome_user_right.components.c_display_options.uppercase_user = $(e.target).prop('checked')
+  // entity_welcome_user_right.components.c_display_options.uppercase_user = $(e.target).prop('checked')
 
   add_or_remove_component(world, 
     $(e.target).prop('checked'), 
@@ -194,7 +194,7 @@ $("input[name=uppercase_user]").change(function(e) {
 })
 
 $("input[name=uppercase_welcome_user]").change(function(e) {
-  entity_welcome_user_right.components.c_display_options.uppercase_welcome_user = $(e.target).prop('checked')
+  // entity_welcome_user_right.components.c_display_options.uppercase_welcome_user = $(e.target).prop('checked')
 
   add_or_remove_component(world, 
     $(e.target).prop('checked'), 
