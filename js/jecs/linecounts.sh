8c0fbbcf6629a9e79950e7b5b4f8eb741a34783a
Andy Bulka <abulka@gmail.com>
2019-07-10 18:00:20 +1000

    updated line counts

---
+++ js/jecs/linecounts.sh
@@ -0,0 +1,11 @@
+echo ---------LINE COUNTS-----------------
+wc -l main_oo.js observer_oo.js 
+echo -------------------------------------
+wc -l main_oo_events.js observer_events.js 
+echo -------------------------------------
+wc -l main_plain.js
+echo -------------------------------------
+wc -l main_vue.js
+echo -------------------------------------
+wc -l main_ecs.js nested_dict_accessor.js
+echo -------------------------------------
