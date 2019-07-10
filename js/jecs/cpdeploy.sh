4652017b682544bf26f660d813bca519d8ea2fbd
Andy Bulka <abulka@gmail.com>
2019-07-10 13:55:56 +1000

    moved research links out of index.html

---
+++ js/jecs/cpdeploy.sh
@@ -0,0 +1,6 @@
+cp assert.js index.html jecs_extras.js jecs_min.js jecs.js main_ecs.html main_ecs.js main_oo.html main_oo.js main_plain.html main_plain.js main_vue.html main_vue.js main.css nested_dict_accessor.js observer_oo.js screenshot01.png syntax_highlighting.js ~/Devel/abulka.github.io/gui-showdown
+
+echo "Now cd into ~/Devel/abulka.github.io/gui-showdown, git commit, git push 
+Then access with
+https://abulka.github.io/gui-showdown/index.html 
+"
--- js/jecs/zipdeploy.sh
+++ js/jecs/zipdeploy.sh
@@ -1,4 +1,4 @@
-zip gui_showdown_deploy.zip assert.js index.html jecs_extras.js jecs_min.js jecs.js main_ecs_v1.html main_ecs_v1.js main_ecs.html main_ecs.js main_oo.html main_oo.js main_oo_v1.html main_oo_v1.js main_plain.html main_plain.js main_vue.html main_vue.js main.css nested_dict_accessor.js observer_oo.js screenshot01.png syntax_highlighting.js
+zip gui_showdown_deploy.zip assert.js index.html jecs_extras.js jecs_min.js jecs.js main_ecs.html main_ecs.js main_oo.html main_oo.js main_plain.html main_plain.js main_vue.html main_vue.js main.css nested_dict_accessor.js observer_oo.js screenshot01.png syntax_highlighting.js
 
 echo "Now upload zip file to atug and unzip into atug-dir/gui-showdown 
 and unzip by clicking on yellow icon in LF control panel. Then access with
