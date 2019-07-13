b47a0ee4c5cc8b34ac1b7a013305c46448862c8c
Andy Bulka <abulka@gmail.com>
2019-07-13 17:19:21 +1000

    cpdeploy script now on multiple lines

--- js/jecs/cpdeploy.sh
+++ js/jecs/cpdeploy.sh
@@ -1,4 +1,15 @@
-cp assert.js index.html jecs_extras.js jecs_min.js jecs.js main_ecs.html main_ecs.js main_oo.html main_oo.js main_plain.html main_plain.js main_vue.html main_vue.js main.css nested_dict_accessor.js observer_oo.js screenshot01.png syntax_highlighting.js ~/Devel/abulka.github.io/gui-showdown
+cp -v assert.js index.html jecs_extras.js jecs_min.js jecs.js \
+main_ecs.html main_ecs.js \
+nested_dict_accessor.js \
+main_oo.html main_oo.js \
+main_oo_events.html main_oo_events.js \
+observer_oo.js observer_oo_events.js \
+main_plain.html main_plain.js \
+main_vue.html main_vue.js \
+main.css \
+screenshot01.png \
+syntax_highlighting.js \
+~/Devel/abulka.github.io/gui-showdown
 
 echo "Now cd into ~/Devel/abulka.github.io/gui-showdown, git commit, git push 
 Then access with
