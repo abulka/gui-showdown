bb1a37417971eca1d26f67fa17bd48af65365f12
Andy Bulka <abulka@gmail.com>
2019-07-14 00:07:57 +1000

    missing file

--- js/jecs/cpdeploy.sh
+++ js/jecs/cpdeploy.sh
@@ -2,7 +2,7 @@ cp -v assert.js index.html jecs_extras.js jecs_min.js jecs.js \
 main_ecs.html main_ecs.js \
 nested_dict_accessor.js \
 main_oo.html main_oo.js \
-observer_oo.js observer_oo_events.js \
+observer_oo.js observer_events.js \
 main_plain.html main_plain.js \
 main_vue.html main_vue.js \
 main.css \
