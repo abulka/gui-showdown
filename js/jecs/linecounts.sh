6c6d8ec49299687643318d7ee610506d26d3dd7e
Andy Bulka <abulka@gmail.com>
2019-07-16 18:35:53 +1000

    updated index and line counts

--- js/jecs/linecounts.sh
+++ js/jecs/linecounts.sh
@@ -1,7 +1,7 @@
 echo ---------LINE COUNTS-----------------
-wc -l main_oo.js observer_oo.js 
+wc -l main_oo_v3_observer.js observer_oo.js 
 echo -------------------------------------
-wc -l main_oo_events.js observer_events.js 
+wc -l main_oo.js observer_events.js 
 echo -------------------------------------
 wc -l main_plain.js
 echo -------------------------------------
