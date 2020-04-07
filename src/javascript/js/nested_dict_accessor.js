f01679198fdaf555b39bf7a44a17023936cf7e4a
Andy Bulka <abulka@gmail.com>
2020-04-07 14:39:24 +1000

    NestedDictAccess comments and lambdas

--- src/javascript/js/nested_dict_accessor.js
+++ src/javascript/js/nested_dict_accessor.js
@@ -1,3 +1,18 @@
+/**
+ * NestedDictAccess - wraps an object 'model' so that setting .val will
+ * get or set the key specified in that object. The parameter 'keys'
+ * is a list of keys, which allows drilling through sub objects/dicts 
+ * during that get or set access.
+ * 
+ * E.g. The keys ["user", "firstname"] will access model.user.firstname
+ * 
+ * Reason for this class? Well, it lets you record what you want to access once
+ * then generic functions can later access that attribute without knowing
+ * specifically what they are accessing. I suppose its like a 
+ * factory function returning a lambda, where
+ * you wrap some behaviour in a function and then users of that function 
+ * simply call that function, not knowing the behaviour.
+ */
 class NestedDictAccess {  // Reference to a shared model object
     constructor(model, keys) {
       this.model = model;  // any object/dict
@@ -24,9 +39,56 @@ class NestedDictAccess {  // Reference to a shared model object
     }
 }
   
-//   // Quick test of NestedDictAccess
-//   m = new NestedDictAccess(model, ["user", "firstname"])
-//   assert(m.val == "Sam")
-//   m.val = "Mary"
-//   assert(m.val == "Mary")
-  
+// Quick test of NestedDictAccess
+// (uncomment all the code below this line)
+// -----------------------------------------------------
+
+// function assert(condition, message) {
+//   if (!condition) {
+//       message = message || "Assertion failed";
+//       if (typeof Error !== "undefined") {
+//           throw new Error(message);
+//       }
+//       throw message; // Fallback
+//   }
+// }
+// var model = {
+//   welcomemsg: "Welcome", 
+//   user: {
+//     firstname: "Sam", 
+//     surname: "Smith"
+//   }
+// }
+// assert(model.user.firstname == "Sam")
+// let m_firstname = new NestedDictAccess(model, ["user", "firstname"])
+// assert(m_firstname.val == "Sam")
+// m_firstname.val = "Mary"
+// assert(m_firstname.val == "Mary")
+// assert(model.user.firstname == "Mary")
+
+// /**
+//  * I suppose it could be done with a functions but have to mention the 
+//  * object name 'obj' each time
+//  */
+// let surname_set = (obj, val) => { obj.user.surname = val }
+// let surname_get = (obj) => { return obj.user.surname }
+// // test
+// assert(model.user.surname == "Smith")
+// assert(surname_get(model) == "Smith")
+// surname_set(model, "Flinstone")  // change it using the lambda
+// assert(model.user.surname == "Flinstone")
+// assert(surname_get(model) == "Flinstone")
+
+// /** 
+//  * I suppose it could be done with lambdas, functions which return a function locked to an obj
+//  */ 
+// let factory_model_surname_set = function (obj) { return (val)=> { obj.user.surname = val }}
+// let factory_model_surname_get = function (obj) { return () => { return obj.user.surname }}
+// // create the lambda functions
+// let m_surname_set = factory_model_surname_set(model)
+// let m_surname_get = factory_model_surname_get(model)
+// // test
+// assert(model.user.surname == "Flinstone")
+// assert(m_surname_get() == "Flinstone")  // lambda approach, notice we don't mention 'model' its baked in
+// m_surname_set("Jones")  // lambda approach, again
+// assert(model.user.surname == "Jones")
