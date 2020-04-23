b3b834d04baa88d99cf087267101f74a72041bdb
Andy Bulka <abulka@gmail.com>
2020-04-23 20:21:41 +1000

    added logging to display - basics

--- research/js/jecs2/basics.js
+++ research/js/jecs2/basics.js
@@ -1,53 +1,89 @@
 // Andy revamp - Apr 2020 - "its gotta be simpler than my first attempt!"
 
-// Instantiating engine, timer and simulator
-var engine = new Jecs.Engine();
+// $(document).ready(function() {
+var app = (function () {
+
+  // Instantiating engine, timer and simulator
+  var engine = new Jecs.Engine();
+    
+  // Declare entities
+  const message = engine.entity('model-welcome-message');
+  const firstname = engine.entity('model-firstname');
+  const surname = engine.entity('model-surname');
+
+  // Associate the model entities to components.
+  message.setComponent('data', {val: "Welcome"})
+  firstname.setComponent('data', {val: "Sam"})
+  surname.setComponent('data', {val: "Smith"})
+
+  message.setComponent('displayOptions', {upper: false})
+
+  function uppercase_message() {
+    message.getComponent('displayOptions').upper = true
+  }
+
+  engine.system('pre-render', ['data', 'displayOptions'], (entity, {data, displayOptions}) => {
+    data.val = displayOptions.upper ? data.val.toUpperCase() : data.val.toLowerCase()
+    // console.log('hi', entity.name, displayOptions, data.val)
+  });
+
+  // Define a 'render' system for updating val of
+  // entities associated to components 'data'
+  engine.system('render', ['data'], (entity, {data}) => {
+    // data.val += "x"
+    console.log('hi', entity.name, data.val)
+    log(`render: ${entity.name}, ${data.val}`);
+  });
+
+
+
+  // util
+
+  var logarea = document.getElementById('log');
+
+  // Append a line of log
+  function log(text) {
+    var html = logarea.innerHTML;
+    html += (text || '') + '<br/>';
+    logarea.innerHTML = html;
+
+    // Scroll log to bottom
+    logarea.scrollTop = logarea.scrollHeight;
+  }
+
+
+
+  engine.tick()
+
+  /*
+
+  // If you prefer, you can avoid using simulator and start
+  // engine iterations manually by calling engine.tick() in a loop.
+  var sim = new Jecs.Simulator(engine);
+
+  // Limit the fps to 6
+  sim.setFps(6);
+
+  // Start simulator
+  sim.start();
+
+  // Stop simulator after three seconds
+  setInterval(function() { sim.stop() }, 3000);
+
+  */
+
+
+// });
+
+  return {
+    // simulator: sim,
+    uppercase_message: uppercase_message,
+    engine: engine,
+    // reset: function() {
+    //   init();
+    //   draw();
+    // },
+  }
   
-// Declare entities
-const message = engine.entity('model-welcome-message');
-const firstname = engine.entity('model-firstname');
-const surname = engine.entity('model-surname');
+}());
 
-// Associate the model entities to components.
-message.setComponent('data', {val: ""})
-firstname.setComponent('data', {val: ""})
-surname.setComponent('data', {val: ""})
-
-message.setComponent('displayOptions', {upper: false})
-
-function uppercase_message() {
-  console.log(message)
-  message.getComponent('displayOptions').upper = true
-}
-// Define a 'render' system for updating val of
-// entities associated to components 'data'
-engine.system('render', ['data'], (entity, {data}) => {
-  data.val += "x"
-  console.log('hi', entity.name, data.val)
-});
-
-engine.system('pre-render', ['data', 'displayOptions'], (entity, {data, displayOptions}) => {
-  data.val = displayOptions.upper ? data.val.toUpperCase() : data.val.toLowerCase()
-  console.log('hi', entity.name, displayOptions, data.val)
-});
-
-
-engine.tick()
-engine.tick()
-
-/*
-
-// If you prefer, you can avoid using simulator and start
-// engine iterations manually by calling engine.tick() in a loop.
-var sim = new Jecs.Simulator(engine);
-
-// Limit the fps to 6
-sim.setFps(6);
-
-// Start simulator
-sim.start();
-
-// Stop simulator after three seconds
-setInterval(function() { sim.stop() }, 3000);
-
-*/
