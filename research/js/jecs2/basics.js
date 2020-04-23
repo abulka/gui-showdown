260c2c7c4d5dca42bdca20d9d8e47c14b7c8205e
Andy Bulka <abulka@gmail.com>
2020-04-23 17:53:39 +1000

    renamed to basics

--- research/js/jecs2/basics.js
+++ research/js/jecs2/basics.js
@@ -4,25 +4,18 @@
 var engine = new Jecs.Engine();
 var sim = new Jecs.Simulator(engine);
   
-// Instantiate a new engine
-// const engine = new Engine();
-
 // Declare entities
 const message = engine.entity('model-welcome-message');
 const firstname = engine.entity('model-firstname');
 const surname = engine.entity('model-surname');
 
-
-// Associate the player entity to components.
-// In this case we set 'position' and 'speed'.
-// player.setComponent('position', { x: 0, y: 0 });
-// player.setComponent('speed', { x: 0.5, y: 0.7 });
+// Associate the model entities to components.
 message.setComponent('data', {val: ""})
 firstname.setComponent('data', {val: ""})
 surname.setComponent('data', {val: ""})
 
-// Define a 'move' system for updating position of
-// entities associated to components 'position' and 'speed'
+// Define a 'render' system for updating val of
+// entities associated to components 'data'
 engine.system('render', ['data'], (entity, {data}) => {
   data.val += "x"
   console.log('hi', entity.name, data.val)
@@ -31,10 +24,11 @@ engine.system('render', ['data'], (entity, {data}) => {
 // If you prefer, you can avoid using simulator and start
 // engine iterations manually by calling engine.tick() in a loop.
 
-// Limit the fps to 60
+// Limit the fps to 6
 sim.setFps(6);
 
 // Start simulator
 sim.start();
 
+// Stop simulator after three seconds
 setInterval(function() { sim.stop() }, 3000);
