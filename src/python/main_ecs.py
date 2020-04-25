5e32fe00bf2fe962dfbf1d7a53ab4ff24a0feda5
Andy Bulka <abulka@gmail.com>
2020-04-25 15:21:55 +1000

    cleanup python version

--- src/python/main_ecs.py
+++ src/python/main_ecs.py
@@ -90,184 +90,10 @@ def display_option_toggle_topright_case(flag):
     engine.component_for_entity(topright, DisplayOptions).upper = flag
 
 
+# Util - Uppercase
 
-# OLD
-
-
-
-# message = world.create_entity(
-#     ModelRef(data=model, keys=["welcomemsg"]), 
-#     GuiControlRef(el=frame.m_staticText1, el_type=GuiElType.label),
-#     DisplayOptions()
-# )
-
-# #
-# # Model - The model is pure data.
-# #
-
-# model = {
-#     "welcomemsg": "Welcome", 
-#     "user": {
-#         "firstname": "Sam", 
-#         "surname": "Smith"
-#     }
-# }
-
-# #
-# # Mediators - are implemented as Entities with a list of data-only Components
-# #
-
-
-# @dataclass
-# class ModelRef(NestedDictAccess):  
-#     """
-#     Dynamically access or set nested dictionary keys in model
-#     Example: m = NestedDictAccess(model, ["user", "firstname"]); print(m.val); m.val = "fox"
-#     Params: 'data' ref to dict and list of 'keys' representing nested path
-#     """
-#     finalstr: str = ""
-
-
-# @dataclass
-# class PlainData:  
-#     finalstr: str = ""
-
-
-# @dataclass
-# class MultiModelRef:  # Refers to multiple model fields, since can only have one component per entity can't have multiple ModelRefs
-#     refs: List[ModelRef]
-
-
-# class GuiElType(Enum):  # Tells render system what kind of wxPython control 'set' value method to use to when rendering
-#     label = 1
-#     textctrl = 2
-#     frametitle = 3
-
-
-# @dataclass
-# class GuiControlRef:  # Mediator (entity + this component) needs to know about a wxPython gui control
-#     el: object        # reference to wxPython control, but could be implemented with 'id' string or something
-#     el_type: GuiElType   
-
-
-# @dataclass
-# class DisplayOptions:
-#     uppercase_welcome: bool = False
-#     uppercase_user: bool = False
-
-
-# @dataclass
-# class ComponentFrameAdornments:
-#     frame_title: str = ""
-#     frame_ref: Any = None
-#     panel_colour: Any = None
-#     panel_ref: Any = None
-#     panel_colour_randomise: bool = False
-
-
-# #
-# # Systems - (ECS behaviour)
-# #
-
-
-# class ModelExtractProcessor(esper.Processor):
-#     """
-#     Extract info from models and stage them in the component in 'finalstr', so that the transform phase
-#     can alter that info without altering the model.  This makes use of multiple 'system' transformations e.g.
-#     and decouples transformation from the final output/rendering system stage.
-#     """
-
-#     def process(self):
-#         print("--Model Extract System---")
-#         for ent, (component, _) in self.world.get_components(ModelRef, Dirty):
-#             component.finalstr = component.val
-#             logsimple(component, ent)
-
-#         for ent, (component, _) in self.world.get_components(MultiModelRef, Dirty):
-#             for model_ref in component.refs:
-#                 model_ref.finalstr = model_ref.val
-
-
-# class CaseTransformProcessor(esper.Processor):
-#     def process(self):
-#         print("--Case transform System---")
-
-#         for ent, (component, display_options, _) in self.world.get_components(ModelRef, DisplayOptions, Dirty):
-#             if display_options.uppercase_welcome and "welcomemsg" in component.keys:
-#                 component.finalstr = component.finalstr.upper()
-#                 logsimple(component, ent)
-
-#         for ent, (component, display_options, _) in self.world.get_components(MultiModelRef, DisplayOptions, Dirty):
-#             for model_ref in component.refs:
-#                 c = model_ref
-#                 if (display_options.uppercase_welcome and "welcomemsg" in c.keys) or \
-#                    (display_options.uppercase_user and ("firstname" in c.keys or "surname" in c.keys)):
-#                     c.finalstr = c.finalstr.upper()
-#                     logsimple(component, ent)
-
-
-# class RenderProcessor(esper.Processor):
-
-#     def render(self, s, gui):  # Helper
-#         if gui.el_type == GuiElType.label:
-#             gui.el.SetLabel(s)
-#         elif gui.el_type == GuiElType.textctrl:
-#             gui.el.SetValue(s)
-#         elif gui.el_type == GuiElType.frametitle:
-#             gui.el.SetTitle(s)
-
-#     def gather_model_refs_into_single_dict(self, c_multi_model_ref):  # Helper
-#         data = {}  # can't target how model ref components get found, so build up multi model output string here, via dict
-#         for ent, (component, gui, _) in self.world.get_components(MultiModelRef, GuiControlRef, Dirty):
-#             for model_ref in component.refs:
-#                 data[model_ref.keys[-1]] = model_ref.finalstr
-#         return data
-
-#     def process(self):
-#         print("--Render System--")
-
-#         for ent, (component, gui, _) in self.world.get_components(ModelRef, GuiControlRef, Dirty):
-#             self.render(component.finalstr, gui)
-
-#         for ent, (component, gui, _) in self.world.get_components(PlainData, GuiControlRef, Dirty):
-#             self.render(component.finalstr, gui)
-
-#         for ent, (component_multi_model_ref, gui, _) in self.world.get_components(MultiModelRef, GuiControlRef, Dirty):
-#             data = self.gather_model_refs_into_single_dict(component_multi_model_ref)
-#             self.render(f"{data['welcomemsg']} {data['firstname']} {data['surname']}", gui)
-
-#         do.dirty_all(False, entities=mediators)
-
-
-# class HousekeepingProcessor(esper.Processor):
-#     def process(self):
-#         print("--Housekeeping System--")
-
-#         if frame.m_checkBox1.IsChecked():
-#             frame.m_checkBox1A.Disable()
-#         else:
-#             frame.m_checkBox1A.Enable()
-
-
-# class FunProcessor(esper.Processor):
-#     def process(self):
-#         print("--Fun System--")
-
-#         for _, (f,) in self.world.get_components(ComponentFrameAdornments):
-#             f.frame_ref.SetTitle(f.frame_title + " " + time.asctime())
-#             f.panel_ref.SetBackgroundColour(
-#                 wx.Colour(255, random.randint(120, 250), random.randint(120, 250)) if f.panel_colour_randomise else f.panel_colour
-#             )
-#             f.panel_ref.Refresh()  # f.panel_ref.Update() doesn't work, need to Refresh()
-
-
-#
-# Util
-#
-
-
-def logsimple(component, entity):  # simple logging
-    print(f"have set {component} for {nice_entity_name[entity]}")
+def toggleCase(s):
+    return s.lower() if s.isupper() else s.upper()
 
 
 #
@@ -348,7 +174,7 @@ class MyFrame1A(MyFrame1):
 
 
 #
-# Init WxPython
+# Init WxPython - do this before defining Systems which need to refer to 'frame'
 #
 
 app = wx.App()
@@ -357,20 +183,9 @@ frame.SetTitle("Gui wired via ECS")
 frame.Show()
 frame.SetSize((500, 450))
 
-#
-# Wire up and build everything
-#
 
+# Systems
 
-
- # Systems
-
-#   engine.system('controller-model', ['data'], (entity, { data }) => {
-#     if (entity.name == 'model-welcome-message') $('input[name=welcome]').val(data.val)
-#     else if (entity.name == 'model-firstname') $('input[name=firstname]').val(data.val)
-#     else if (entity.name == 'model-surname') $('input[name=surname]').val(data.val)
-#     log(`controller-model: ${entity.name}, ${data.val}`);
-#   });
 
 class ControllerModel(esper.Processor):
     """Render model into input fields"""
@@ -419,106 +234,11 @@ class ControllerTopRight(esper.Processor):
             frame.m_staticText2.SetLabel(s)
             print(f"controller-topright: {entity_name[entity]}, {rdata}, {displayOptions}")
 
-  
-
-
 
 engine.add_processor(ControllerModel())
 engine.add_processor(PreRenderTopRight())
 engine.add_processor(ControllerTopLeft())
 engine.add_processor(ControllerTopRight())
-
-
-
-
-
-
-
-
-
-
-
-# OLD
-
-
-# world = esper.World()
-# world.add_processor(ModelExtractProcessor())
-# world.add_processor(CaseTransformProcessor())
-# world.add_processor(RenderProcessor())
-# world.add_processor(HousekeepingProcessor())
-# world.add_processor(FunProcessor())
-
-# entity_welcome_left = world.create_entity(
-#     ModelRef(data=model, keys=["welcomemsg"]), 
-#     GuiControlRef(el=frame.m_staticText1, el_type=GuiElType.label),
-#     DisplayOptions()
-# )
-
-# entity_welcome_user_right = world.create_entity(
-#     MultiModelRef(refs=[
-#         ModelRef(data=model, keys=["welcomemsg"]),
-#         ModelRef(data=model, keys=["user", "firstname"]),
-#         ModelRef(data=model, keys=["user", "surname"]),
-#         ]),
-#     GuiControlRef(el=frame.m_staticText2, el_type=GuiElType.label),
-#     DisplayOptions()
-# )
-
-# entity_edit_welcome_msg = world.create_entity(
-#     ModelRef(data=model, keys=["welcomemsg"]), 
-#     GuiControlRef(el=frame.m_textCtrl1, el_type=GuiElType.textctrl)
-# )
-# entity_edit_user_name_msg = world.create_entity(
-#     ModelRef(data=model, keys=["user", "firstname"]), 
-#     GuiControlRef(el=frame.m_textCtrl2, el_type=GuiElType.textctrl)
-# )
-# entity_edit_user_surname_msg = world.create_entity(
-#     ModelRef(data=model, keys=["user", "surname"]), 
-#     GuiControlRef(el=frame.m_textCtrl3, el_type=GuiElType.textctrl)
-# )
-# entity_page_title = world.create_entity(
-#     PlainData("Gui wired via ECS (Entity Component System)"), 
-#     GuiControlRef(el=frame, el_type=GuiElType.frametitle)
-# )
-# appgui = world.create_entity()  # slightly different style, create entiry then add components
-# world.add_component(
-#     appgui,
-#     ComponentFrameAdornments(
-#         frame_title="Gui wired via ESC",
-#         frame_ref=frame,
-#         panel_colour=wx.Colour(255, 255, 135),
-#         panel_ref=frame.m_panel1,
-#         panel_colour_randomise=True,
-#     ),
-# )
-
-# nice_entity_name = {
-#     entity_welcome_left: "mediator for welcome_left",
-#     entity_welcome_user_right: "mediator for welcome_user_right",
-#     entity_edit_welcome_msg: "mediator for edit_welcome_msg",
-#     entity_edit_user_name_msg: "mediator for edit_user_name_msg",
-#     entity_edit_user_surname_msg: "mediator for edit_user_surname_msg",
-#     entity_page_title: "mediator for entity_page_title",
-#     appgui: "mediator for app frame etc",
-# }
-# mediators: List[int] = list(nice_entity_name.keys())
-
-# # Observer Wiring
-# do = DirtyObserver(world)
-
-# do.add_dependency(ModelRef, [entity_welcome_left, entity_edit_welcome_msg, entity_edit_user_name_msg, entity_edit_user_surname_msg])
-# do.add_dependency(MultiModelRef, [entity_welcome_user_right])
-# do.add_dependency("welcome display only, not via model", [entity_welcome_left, entity_welcome_user_right])
-# do.add_dependency("user display only, not via model", [entity_welcome_user_right])
-# do.add_dependency("just top right", [entity_welcome_user_right])
-
-# do.dirty_all(entities=mediators)  # initialise the gui with initial model values
-
-
-# Util - Uppercase
-
-def toggleCase(s):
-    return s.lower() if s.isupper() else s.upper()
 
 world.process()
 
