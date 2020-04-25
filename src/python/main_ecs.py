330ad3aecc89a09f0db08713c861453453de5502
Andy Bulka <abulka@gmail.com>
2020-04-25 15:17:49 +1000

    ecs python revamp works ok

--- src/python/main_ecs.py
+++ src/python/main_ecs.py
@@ -19,6 +19,14 @@ firstname = engine.create_entity('model-firstname')
 surname = engine.create_entity('model-surname')
 topright = engine.create_entity('display-model-topright')
 
+# Map entities (which are just id's) into meaningful names for debugging
+entity_name = {
+    message: "message",
+    firstname: "firstname",
+    surname: "surname",
+    topright: "topright",
+}
+
 # Components - simple, pure data
 @dataclass
 class Data:
@@ -68,6 +76,18 @@ def toggle_user():
     data = engine.component_for_entity(surname, Data)
     data.val = toggleCase(data.val)
 
+
+# App - Display Option Checkbox toggles
+
+def display_option_toggle_message_case(flag):
+    engine.component_for_entity(message, DisplayOptions).upper = flag
+
+def display_option_toggle_user_case(flag):
+    engine.component_for_entity(firstname, DisplayOptions).upper = flag
+    engine.component_for_entity(surname, DisplayOptions).upper = flag
+
+def display_option_toggle_topright_case(flag):
+    engine.component_for_entity(topright, DisplayOptions).upper = flag
 
 
 
@@ -269,72 +289,59 @@ class MyFrame1A(MyFrame1):
 
     # Overridden events
 
-    def on_button_reset_welcome(self, event):
-        # model["welcomemsg"] = "Hello"
-        # do.dirty(ModelRef, lambda component : "welcomemsg" in component.keys, filter_nice_name="welcomemsg")
-        # do.dirty(MultiModelRef)
+    # Which change the actual model - input fields
+
+    def on_enter_welcome(self, event):
+        set_message(event.GetEventObject().GetValue())
         world.process()
 
-    def on_button_reset_user(self, event):
-        # model["user"]["firstname"] = "Fred"
-        # model["user"]["surname"] = "Flinstone"
-        # do.dirty(ModelRef)  # could optimise with filters but don't bother, only one will be redundant
-        # do.dirty(MultiModelRef)
+    def on_enter_user_firstname(self, event):
+        set_firstname(event.GetEventObject().GetValue())
         world.process()
+
+    def on_enter_user_surname(self, event):
+        set_surname(event.GetEventObject().GetValue())
+        world.process()
+
+    # Which change the actual model - buttons
 
     def on_button_change_welcome_model_case(self, event):
         # toggle the case of the model's welcome message
-        # model["welcomemsg"] = model["welcomemsg"].upper() if model["welcomemsg"][1].islower() else model["welcomemsg"].lower()
-        # do.dirty(ModelRef, lambda component : "welcomemsg" in component.keys, filter_nice_name="welcomemsg")
-        # do.dirty(MultiModelRef)
         toggle_message()
         world.process()
 
     def on_button_change_user_model_case(self, event):
         # toggle the case of the model's user message
-        # model["user"]["firstname"] = model["user"]["firstname"].upper() if model["user"]["firstname"][1].islower() else model["user"]["firstname"].lower()
-        # model["user"]["surname"] = model["user"]["surname"].upper() if model["user"]["surname"][1].islower() else model["user"]["surname"].lower()
-        # do.dirty(ModelRef, lambda component : "firstname" in component.keys or "surname" in component.keys, filter_nice_name="firstname or surname")
-        # do.dirty(MultiModelRef)
+        toggle_user()
         world.process()
+    
+    def on_button_reset_welcome(self, event):
+        set_message("Hello")
+        world.process()
+
+    def on_button_reset_user(self, event):
+        set_firstname("Fred")
+        set_surname("Flinstone")
+        world.process()
+
+    # Which change the way the model is displayed at the top left and top right, but does not change the model
 
     def on_check_toggle_welcome_outputs_only(self, event):
         # toggle the case of the welcome output messages only - do not affect model
-        # world.component_for_entity(entity_welcome_left, DisplayOptions).uppercase_welcome = event.GetEventObject().IsChecked()
-        # world.component_for_entity(entity_welcome_user_right, DisplayOptions).uppercase_welcome = event.GetEventObject().IsChecked()
-        # do.dirty("welcome display only, not via model")  # doesn't affect welcome text edit field
+        display_option_toggle_message_case(event.GetEventObject().IsChecked())
         world.process()
 
     def on_check_toggle_user_outputs_only(self, event):
         # toggle the case of the user output messages only - do not affect model
-        # world.component_for_entity(entity_welcome_user_right, DisplayOptions).uppercase_user = event.GetEventObject().IsChecked()
-        # do.dirty("user display only, not via model")  # doesn't affect welcome text edit field
+        display_option_toggle_user_case(event.GetEventObject().IsChecked())
         world.process()
 
     def on_check_upper_entire_top_right_output(self, event):
         # toggle the case of both the welcome and user output messages only on rhs (top right) display - do not affect model
-        # world.component_for_entity(entity_welcome_user_right, DisplayOptions).uppercase_welcome = event.GetEventObject().IsChecked()
-        # world.component_for_entity(entity_welcome_user_right, DisplayOptions).uppercase_user = event.GetEventObject().IsChecked()
-        # do.dirty("just top right")
+        display_option_toggle_topright_case(event.GetEventObject().IsChecked())
         world.process()
 
-    def on_enter_welcome(self, event):
-        # model["welcomemsg"] = event.GetEventObject().GetValue()
-        # do.dirty(ModelRef, lambda component : "welcomemsg" in component.keys, filter_nice_name="welcomemsg")
-        # do.dirty(MultiModelRef)
-        world.process()
-
-    def on_enter_user_firstname(self, event):
-        # model["user"]["firstname"] = event.GetEventObject().GetValue()
-        # do.dirty(ModelRef, lambda component : "firstname" in component.keys, filter_nice_name="firstname")
-        # do.dirty(MultiModelRef)
-        world.process()
-
-    def on_enter_user_surname(self, event):
-        # model["user"]["surname"] = event.GetEventObject().GetValue()
-        # do.dirty(ModelRef, lambda component : "surname" in component.key, filter_nice_name="surname")
-        # do.dirty(MultiModelRef)
-        world.process()
+    # Debug
 
     def onClickRenderNow(self, event):
         world.process()
@@ -374,18 +381,52 @@ class ControllerModel(esper.Processor):
             elif entity == surname: frame.m_textCtrl3.SetValue(data.val)
             print(f"controller-model: {entity_name[entity]}, {data.val}")
 
+class PreRenderTopRight(esper.Processor):
+    """Gather/buffer partial info from individual entities/models into a 
+    final 'display model' entity for later render at top right of GUI
+    """
+    def process(self):
+        for entity, (data, displayOptions) in self.world.get_components(Data, DisplayOptions):
+            buffer = engine.component_for_entity(topright, RenderData)
+            if entity == message:
+                buffer.welcome = data.val.upper() if displayOptions.upper else data.val
+            elif entity == firstname:
+                buffer.firstname = data.val.upper() if displayOptions.upper else data.val
+            elif entity == surname:
+                buffer.surname = data.val.upper() if displayOptions.upper else data.val
+            print(f"pre-render-topright: {entity_name[entity]}, {data.val}, {displayOptions}")
 
-# Map entities (which are just id's) into meaningful names for debugging
-entity_name = {
-    message: "welcome message",
-    firstname: "firstname",
-    surname: "surname",
-    topright: "topright",
-}
+class ControllerTopLeft(esper.Processor):
+    """Gather/buffer partial info from individual entities/models into a 
+    final 'display model' entity for later render at top right of GUI
+    """
+    def process(self):
+        for entity, (data, displayOptions) in self.world.get_components(Data, DisplayOptions):
+            if entity == message:
+                s = data.val.upper() if displayOptions.upper else data.val
+                frame.m_staticText1.SetLabel(s)
+                print(f"controller-topleft: {entity_name[entity]}, {data.val}, {displayOptions}")
+
+class ControllerTopRight(esper.Processor):
+    """Gather/buffer partial info from individual entities/models into a 
+    final 'display model' entity for later render at top right of GUI
+    """
+    def process(self):
+        for entity, (rdata, displayOptions) in self.world.get_components(RenderData, DisplayOptions):
+            s = f"{rdata.welcome} {rdata.firstname} {rdata.surname}"
+            if displayOptions.upper:
+                s = s.upper()
+            frame.m_staticText2.SetLabel(s)
+            print(f"controller-topright: {entity_name[entity]}, {rdata}, {displayOptions}")
+
+  
 
 
 
 engine.add_processor(ControllerModel())
+engine.add_processor(PreRenderTopRight())
+engine.add_processor(ControllerTopLeft())
+engine.add_processor(ControllerTopRight())
 
 
 
